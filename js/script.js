d40d23c0627dd8a182d86385e3d5981c98ba7cca
Grant Long <elginowls2020@gmail.com>
2024-03-28 18:24:11 -0500

    Added the anonymous post functionality and character count to textbox

--- js/script.js
+++ js/script.js
@@ -5,3 +5,29 @@ menuToggle.addEventListener('click', () => {
   navLinks.classList.toggle('active');
 });
 
+function updateCharacterCount(element) {
+  const charCountSpan = document.getElementById('charCount');
+  const currentLength = element.value.length;
+  charCountSpan.textContent = `${currentLength}/5000 characters`;
+}
+function toggleFields() {
+  const checkbox = document.getElementById('defaultCheck1');
+  const firstNameInput = document.getElementById('firstname');
+  const lastNameInput = document.getElementById('lastname');
+  const jobTitleInput = document.getElementById('jobtitle');
+  const emailInput = document.getElementById('email');
+
+  if (checkbox.checked) {
+      // Disable the input fields
+      firstNameInput.disabled = true;
+      lastNameInput.disabled = true;
+      jobTitleInput.disabled = true;
+      emailInput.disabled = true;
+  } else {
+      // Enable the input fields
+      firstNameInput.disabled = false;
+      lastNameInput.disabled = false;
+      jobTitleInput.disabled = false;
+      emailInput.disabled = false;
+  }
+}
